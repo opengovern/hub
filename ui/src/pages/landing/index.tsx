b54d3c318af7d5e1ba1aa91391fdafa5f7f9f528
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-12-28 13:29:51 +0330

    feat: remove use cases

--- ui/src/pages/landing/index.tsx
+++ ui/src/pages/landing/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       {/* <GlobalDatabase /> */}
       <Features />
       {/* <UseCase /> */}
-      <UseCaseNew />
+      {/* <UseCaseNew /> */}
       <Steps />
 
       <CodeExample />
